52d69fc4a1d5f281245bef0f16378c5e5e95cc66
Наталья Романова <Natalocka_ne@mail.ru>
2024-02-10 14:08:36 +0300

    completed the second part of the task

---
+++ src/App.js
@@ -0,0 +1,80 @@
+import { useForm } from "react-hook-form";
+import * as yup from 'yup';
+import { yupResolver } from "@hookform/resolvers/yup";
+import styles from './App.module.css';
+import { useRef } from "react";
+
+const sendFormData = (formData) => {
+	console.log(formData)
+};
+
+const fieldsSchema = yup.object()
+	.shape({
+		email: yup.string()
+			.required('Поле email обязательно для заполнения')
+			.matches(/^[A-Z0-9._%+-]+@[A-Z0-9-]+\.[A-Z]{2,}$/i, 'Некорректный email. Ведите почту в формате: yorPostAddress@mail.ru'),
+		password: yup.string()
+			.required('Поле пароля обязательно для заполнения')
+			.min(3, 'Некорректый пароль. Пароль должен содержать не менее 3 символов')
+			.max(8, 'Некорректый пароль. Пароль должен содержать не более 8 символов'),
+		repeatPassword: yup.string()
+			.required('Повторите пароль')
+			.oneOf([yup.ref('password'), null], 'Пароли должны совпадать')
+	});
+
+export const App = () => {
+	const {
+		register,
+		handleSubmit,
+		formState: {isDirty, isValid, errors},
+	} = useForm({
+		defaultValues: {
+			email: '',
+			password: '',
+			repeatPassword: '',
+		},
+		resolver: yupResolver(fieldsSchema),
+	});
+
+	const emailError = errors.email?.message;
+	const passwordError = errors.password?.message;
+	const repeatPasswordError = errors.repeatPassword?.message;
+
+	const submitButtonRef = useRef(null);
+
+	if (!isDirty && !isValid) {
+		submitButtonRef.current.focus();
+	}
+
+	return (
+		<div className={styles.app}>
+			<form className={styles.form} onSubmit={handleSubmit(sendFormData)} autoComplete="off">
+				<label className={styles.label}>Форма регистрации</label>
+				{emailError && <div className={styles.errorLabel}>{emailError}</div>}
+				<input className={styles.item}
+					   type="email"
+					   placeholder="Введите email"
+					   {...register('email')}
+				/>
+				{passwordError && <div className={styles.errorLabel}>{passwordError}</div>}
+				<input
+					className={styles.item}
+					type="password"
+					placeholder="Введите пароль"
+					{...register('password')}
+				/>
+				{repeatPasswordError && <div className={styles.errorLabel}>{repeatPasswordError}</div>}
+				<input
+					className={styles.item}
+					name="repeatPassword"
+					type="password"
+					placeholder="Повторите ваш пароль"
+					{...register('repeatPassword')}
+				/>
+				<button className={styles.btn} ref={submitButtonRef} type="submit"
+						disabled={!isDirty || !isValid}>Зарегистрироваться
+				</button>
+			</form>
+		</div>
+	);
+};
